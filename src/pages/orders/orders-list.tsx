6d12f911e6a0dda7fd57e448489f71ade0998236
Andy Nguyen <an.nguyen@devsamurai.com>
2020-08-11 01:04:54 +0700

    feat: update template

--- src/pages/orders/orders-list.tsx
+++ src/pages/orders/orders-list.tsx
@@ -3,6 +3,10 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
 import { useState } from 'react';
 import {useAllOrderQuery, useCancelOrderQuery, useCreateOrderQuery, useGetOrderInformationQuery} from "./order.queries";
 
@@ -25,7 +29,7 @@ const columns = [{
 export const OrderList = () => {
     const {data: allOrders, status} = useAllOrderQuery();
 
-    const [item, setItem] = useState('Test value');
+    const [item, setItem] = useState('');
     const [createOrder, showCreateOrder] = useState(false);
 
     // handle 1
@@ -50,7 +54,8 @@ export const OrderList = () => {
     const [orderDetail, setOrderDetail] = useState(false);
     const [order, setOrder] = useState({
         _id: '',
-        status: ''
+        status: '',
+        userId: ''
     });
     const showOrderDetails = (orderId: string) => {
         getOrderDetailQuery(orderId).then((value) => {
@@ -65,8 +70,6 @@ export const OrderList = () => {
     const [cancelOrderQuery] = useCancelOrderQuery();
     const cancelOrder = () => {
         cancelOrderQuery(order._id).then((value) => {
-           /* setOrder(value);
-            setOrderDetail(true)*/
             closeOrderDetails();
         });
     };
@@ -74,37 +77,81 @@ export const OrderList = () => {
     // handle events
     const tableRowEvents  = {
         onClick: (e: any, row: any, rowIndex: any) => {
-            console.log(`clicked on row with index: ${rowIndex}`);
-            // showCreateOrder(true);
-            console.log(row)
+            console.log(row);
             showOrderDetails(row._id);
         },
     };
 
     return (
         <div>
+            <Container fluid="md">
+                <Row style={{
+                    marginTop: '20px'
+                }}>
+                    <Col>
+                        <h2>
+                            Order List
+                        </h2>
+                    </Col>
+                </Row>
 
-            <Button variant="primary" onClick={showCreateOrderModal}>
-                Create order
-            </Button>
+                <Row>
+                    <Col>Mock User Id: mockUserId</Col>
+                    <Col>Mock Pin Token: mockPinTokenAndMakeItSecreted</Col>
+                </Row>
+                <Row style={{
+                    marginTop: '20px'
+                }}>
+                    <Col>
+                    <Alert variant='primary'>
+                        Click on a row to view order details. An order could be canceled if its status is created or confirmed. After 10s, its status will be delivered. Press F5 to reload if you get some trouble. Thanks!
+                    </Alert>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button  variant="primary" onClick={showCreateOrderModal}>
+                            + Create order
+                        </Button>
+                    </Col>
+                </Row>
+                <Row style={{
+                    marginTop: '20px'
+                }}>
+                    <Col>
+                        { status === 'success' && (
+                            <BootstrapTable keyField='id' data={ allOrders } columns={ columns } rowEvents={tableRowEvents}/>
+                        ) }
+                    </Col>
+                </Row>
+            </Container>
 
-            { status === 'success' && (
-                <BootstrapTable keyField='id' data={ allOrders } columns={ columns } rowEvents={tableRowEvents}/>
-            ) }
-
-
+            {/*/ Order detail modal /*/}
             <Modal show={orderDetail} onHide={closeOrderDetails}>
                 <Modal.Header closeButton>
                     <Modal.Title>Order detail</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    {order._id}
-                    {(order.status === 'CONFIRMED' || order.status === 'CREATED') &&
-                        <Button variant="warning"
-                                onClick={ cancelOrder}>
-                            Cancel Order
-                        </Button>
-                    }
+                    <Container style={{
+                        marginTop: '20px'
+                    }}>
+                        <Row><Col>ID: { order._id }</Col></Row>
+                        <Row><Col>UserId: { order.userId }</Col></Row>
+                        <Row><Col>Status: { order.status }</Col></Row>
+                        <Row style={{
+                            marginTop: '10px'
+                        }}>
+                            <Col>
+                                {(order.status === 'CONFIRMED' || order.status === 'CREATED') &&
+                                <Button variant="warning"
+                                        onClick={ cancelOrder}>
+                                    Cancel Order
+                                </Button>
+                                }
+                            </Col>
+                        </Row>
+                    </Container>
+
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={closeOrderDetails}>
@@ -113,14 +160,14 @@ export const OrderList = () => {
                 </Modal.Footer>
             </Modal>
 
-
+            {/*// create order modal */}
             <Modal show={createOrder} onHide={closeCreateOrderModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Create order</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                         <Form.Group controlId="formItemName">
-                            <Form.Label>Item name</Form.Label>
+                            <Form.Label>Item</Form.Label>
                             <Form.Control type="text" placeholder="Enter item name" value={item}
                                           onChange={handleChange}/>
                         </Form.Group>
